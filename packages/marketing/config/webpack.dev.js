4b53c4f5f4167730f5cfb15969aaf660f8e86cb5
epp1pl <johannes.epp@de.bosch.com>
2024-02-16 17:06:58 +0100

    Container deploy script

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,8 +1,6 @@
 const {merge} = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-const {Module} = require('webpack');
 const packageJson = require('../package.json');
 
 const devConfig = {
@@ -21,9 +19,6 @@ const devConfig = {
                 './MarketingApp': './src/bootstrap'
             },
             shared: packageJson.dependencies,
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
         })
     ]
 }
