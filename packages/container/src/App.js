ea2efcbc48de40b1033d051149ec353dfae33938
epp1pl <johannes.epp@de.bosch.com>
2024-02-16 16:21:51 +0100

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Switch, Route, BrowserRouter}from 'react-router-dom';
+import {StylesProvider} from '@material-ui/core/styles';
+
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+    return <div>
+    <h1>Hi there</h1>
+    <hr/>
+    <MarketingApp />
+    
+    </div>
+};
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+// mount function to start up the app
+const mount = (el) => {
+    ReactDOM.render(
+        <App />,
+        el
+    )
+    ;
+};
+
+
+// if we are in development andn in isolation call mount immediately
+if(process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if(devRoot){
+        mount(devRoot);
+    }
+}
+
+
+export {mount};
+
