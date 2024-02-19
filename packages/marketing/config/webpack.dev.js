aee05708cc1083a66d028758ef32cb557ece6e8f
epp1pl <johannes.epp@de.bosch.com>
2024-02-19 20:55:57 +0100

    Added vue dashboard

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,4 +1,5 @@
-const {merge} = require('webpack-merge');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -6,25 +7,27 @@ const packageJson = require('../package.json');
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: 'http://localhost:8081/'
+    publicPath: 'http://localhost:8081/',
   },
   devServer: {
     port: 8081,
-    historyApiFallback: /*can also be: historyApiFallback: true,*/{
-      index: 'index.html'
-    }
+    historyApiFallback: {
+      historyApiFallback: true,
+    },
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
       filename: 'remoteEntry.js',
       exposes: {
-        './MarketingApp': './src/bootstrap'
+        './MarketingApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
-    })
-    ]
-}
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, devConfig);
-
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createMemoryHistory, createBrowserHistory} from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, {onNavigate, defaultHistory}) => {
-  const history = defaultHistory || createMemoryHistory(); // use defaultHistory if running in isolation mode...
-  if(onNavigate){
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
+
+  if (onNavigate) {
     history.listen(onNavigate);
   }
 
-  ReactDOM.render(<App history={history}/>, el);
+  ReactDOM.render(<App history={history} />, el);
 
   return {
-    onParentNavigate({pathname: nextPathname}) {
-      const {pathname } = history.location;
-      if(pathname !== nextPathname){
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
-    }
-  }
+    },
+  };
 };
 
 // If we are in development and in isolation,
@@ -28,7 +34,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot, {defaultHistory: createBrowserHistory()});
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
